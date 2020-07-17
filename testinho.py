f8cd8683740a83bf15b7a82a5a069ad29b8ef46e
Gabriela <gabriela.rocha71@etecia.com.br>
2020-07-17 15:56:14 -0300

    Todos os exercicios, de facil resolução

---
+++ testinho.py
@@ -0,0 +1,127 @@
+#x= int (input('digite um numero'))
+#y=int(input('digite um segundo numero '))
+#z= x+y
+#print('a soma entre {0} e {1} vale {2}'.format(x, y ,z))
+#---------------------------------------------------------------------
+                          # SUCESSOR E ANTECESSSOR  
+#x= int (input('digite um numero'))
+#y= x+1
+#z=x-1
+#print('Numero digitado',x,'seu sucessor',y,'seu antecessor',z)
+#-------------------------------------------------------------------
+                        #DOBRO DO NUMERO
+#x= int (input('digite um numero'))
+#y= x*2
+#print('Numero digitado {} seu dobro {}'.format(x,y))
+#-------------------------------------------------------------------
+                        #DOBRO TRIPLO RAIZ QUADRADA
+#x=int(input('dgite um numero'))
+#d= x*2
+#t= x*3
+#r= x ** (1/2)
+#print('o dobro de: {} \n vale{}'.format(x,d))
+#print('o triplo de {}\n vale {} \n sua raiz quadrada vale:{}'.format(x,t,r))
+#-------------------------------------------------------------------
+                        #MEDIA DO ALUNO
+#nome=input ('Digite o nome do aluno:')
+#x= float (input('digite a primeira nota do estudante:'))
+#y=float(input('digite a segunda nota do estudante:'))
+#z= x+y
+#a= z/2;
+#print('{} teve a primeira nota de {} e a segunda de {} totalizando a media em {}'
+#     .format(nome,x,y,a))
+#------------------------------------------------------------------------
+                    #NÃO LEMBRO DAS MEDIDAS ENTÃO FOI SO TESTE
+#media=float(input('digite uma distancia em metros'))
+#hm = media * 100
+#dam = media *10
+#dm = media * 10
+#cm = media * 10
+#mm = media/1000
+#print('result km {}\n hm {}\ndam{}\n dm {}\n cm{} \n mm{}'.format(km,hm,dam,dm,cm,mm))
+#------------------------------------------------------------------------
+#x = int(input('digite um numero para ver sua tabuada:'))
+#print('{} x {} = {}'. format (x, 1, x*1 ))
+#----------------------------------
+                        #CONVERSÃO DE MOEDAS
+#real=float(input('digite o quanto voce, para ser convertido'))
+#dolar= real/5.33
+#euro=real/6.07 
+#print('A quantidade que voce tem R${}\n equivale á U${}\n e a {} euros'. format(real,dolar,euro))
+#------------------------------------------------------------------------------
+                        #PAREDE
+#l=float(input('Qual a largura da parede que deseja pintar: '))
+#a=float(input('e qual a altura?'))
+#area=l*a
+#tinta= area/2
+#print('Sua parede tem as medidas de {} x {} então sua area é de {} m²'.format(l,a,area))
+#print('Para pinta-la será necessario {} de tinta'.format(tinta))
+#----------------------------------------------------------------------------
+                          #DESCONTOS
+#x=float (input('Digite o preço do produto: '))
+#y =int(input('Qual a porcentagem de desconto, por favor, insira somente numeros: '))
+#p =x*y/100
+#final=x-p
+#print('O produto tem o valor inteiro de {} com o desconto de {} % tem uma diminuição de R${}\n  passa a custar somente R${}'.format(x,y,p,final))
+#----------------------------------------------------------------------------
+                #AUMENTO DE SALARIO
+#x=float(input('Salario do funcionario: '))
+#y=int(input('Porcentagem do aumento: '))
+#z=x*y/100
+#atual=x+z
+#print('O antigo salario era de {} com o aumento de {} passara a ser {}'.format(x,y,atual))
+#----------------------------------------------------------------------------
+                #JUST TRY
+#name=input('qual o produto: ')
+#x=float(input('Qual o preço do produto: '))
+#y= x*5/100
+#z=x-y
+#j=x*10/100
+#juros=x+j
+#print('{} a vista {} a prazo {}'.format(name,z,juros))
+#----------------------------------------------------------------------------
+                #CONVERSOR DE TEMPERATURA
+#x= float(input('Qual a temperatura °C: '))
+#conv=9*x/5+32
+#print('se esta {}°C logo em °F fica: {}°F'.format(x,conv))
+#----------------------------------------------------------------------------
+                    #ALUGUEL DE CARROS
+#x=float(input('Quanto km foram rodados? '))
+#y=int(input('quantos dias voce ficou com o carro'))
+#km=x *0.15
+#dias= y*60
+#result= km+dias
+#print('O valor a pagar sera de {}'.format(result))
+#----------------------------------------------------------------------------
+                    #SORTEANDO NUMERO ALEATORIO#
+#import random
+#num= random.randint(1,50)
+#print('sera sorteado um numero de 1 a 50.Boa sorte\n numero sorteado é: {}'.format(num))
+#-------------------------------------------------------------------------
+#import emoji
+#print (emoji.emojize("HELLO WORLD",use_aliases=True))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
